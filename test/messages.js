a096811f7562235c4c51f6aeb6bf9de313406a83
cyberfunk <hello@cyberfunk.org>
2013-06-23 22:48:33 +0200

    Add test for specifying a custom error message.

---
+++ test/messages.js
@@ -0,0 +1,17 @@
+var util = require('util');
+var assert = require('chai').assert;
+var schema = require('../index');
+var ValidationError = schema.error;
+
+suite("Custom message validation:", function() {
+  test("validate using a custom error message", function() {
+    var descriptor = {
+      name: {type: "string", required: true, message: "Name is required"},
+    }
+    var validator = new schema(descriptor);
+    validator.validate({}, function(errors, fields) {
+      assert.equal(errors.length, 1);
+      assert.equal(errors[0].message, "Name is required");
+    });
+  });
+});
