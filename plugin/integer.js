84f2557125ea8875dde7c476d73a6adbfb5605fd
muji <freeformsystems@gmail.com>
2015-09-11 12:13:58 +0800

    Use scope in plugins.

--- plugin/integer.js
+++ plugin/integer.js
@@ -5,22 +5,18 @@
  *  @param cb The callback function.
  */
 function integer(opts, cb) {
-  var errors = opts.errors
-    , rule = opts.rule
-    , value = opts.value
-    , source = opts.source
-    , validate = rule.required
-        || (!rule.required && source.hasOwnProperty(rule.field));
+  var validate = this.rule.required
+        || (!this.rule.required && this.source.hasOwnProperty(this.rule.field));
 
   if(validate) {
-    if(value === undefined && !rule.required) {
+    if(this.value === undefined && !this.rule.required) {
       return cb();
     }
-    opts.required();
-    opts.type();
-    opts.range();
+    this.required();
+    this.type();
+    this.range();
   }
-  cb(errors);
+  cb(this.errors);
 }
 
 module.exports = function() {
