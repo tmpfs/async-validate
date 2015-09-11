4b558377b87edb586f9d7b42ea302a16a93efae3
muji <freeformsystems@gmail.com>
2015-09-11 14:47:05 +0800

    Improve validator and schema assertions.

---
+++ test/spec/schema.js
@@ -0,0 +1,55 @@
+var expect = require('chai').expect
+  , schema = require('../../index');
+
+describe("async-validate:", function() {
+
+  it("should error with no rules", function(done) {
+    function fn() {
+      var validator = new schema(); 
+    }
+    expect(fn).throws(Error);
+    expect(fn).throws(/cannot configure/i);
+    expect(fn).throws(/with no rules/i);
+    done();
+  });
+
+  it("should error with rules as array", function(done) {
+    function fn() {
+      var validator = new schema([]); 
+    }
+    expect(fn).throws(Error);
+    expect(fn).throws(/rules must be an object/i);
+    done();
+  });
+
+  it("should error on validate with no rules", function(done) {
+    var validator = new schema({}); 
+    function fn() {
+      validator.validate({});
+    }
+    expect(fn).throws(Error);
+    expect(fn).throws(/cannot validate with no rules/i);
+    done();
+  });
+
+  it("should error on validate with no source", function(done) {
+    var validator = new schema({name: function(cb){cb(this.errors)}}); 
+    function fn() {
+      validator.validate();
+    }
+    expect(fn).throws(Error);
+    expect(fn).throws(/cannot validate with no source/i);
+    done();
+  });
+
+  it("should error on validate with no callback", function(done) {
+    var validator = new schema({name: function(cb){cb(this.errors)}}); 
+    function fn() {
+      validator.validate({});
+    }
+    expect(fn).throws(Error);
+    expect(fn).throws(/cannot validate with no callback/i);
+    done();
+  });
+
+});
---
+++ test/spec/validator.js
@@ -0,0 +1,19 @@
+var expect = require('chai').expect
+  , Validator = require('../../lib/validator');
+
+describe("async-validate:", function() {
+
+  it("should create class without constructor", function(done) {
+    var v = Validator.Type({});
+    expect(v instanceof Validator.Type).to.eql(true);
+    done();
+  });
+
+  it("should use default message", function(done) {
+    var v = Validator.Type({field: 'mock', rule: {}});
+    // trigger default message code path
+    v.error();
+    done();
+  });
+
+});
