1dbfb6bae28181908428d518fce561fc8be81a02
muji <freeformsystems@gmail.com>
2015-09-11 16:40:52 +0800

    Improve test spec.

--- test/spec/message-object.js
+++ test/spec/message-object.js
@@ -5,7 +5,7 @@ var msg = require('../../messages');
 
 describe("async-validate:", function() {
 
-  it("should validate using a custom error message", function(done) {
+  it("should validate using a custom error message as object", function(done) {
     var descriptor = {
       num: {
         type: "number", min: 0, max: 10,
@@ -19,6 +19,24 @@ describe("async-validate:", function() {
     validator.validate({num: -1}, function(errors, fields) {
       assert.equal(errors.length, 1);
       assert.equal(errors[0].message, "Number may not be below zero");
+      done();
+    });
+  });
+
+  it("should validate using a custom error message as object", function(done) {
+    var descriptor = {
+      num: {
+        type: "number", min: 0, max: 10,
+        message: {
+          min: 'Number may not be below zero',
+          max: 'Number may not be above 10',
+        }
+      }
+    }
+    var validator = new schema(descriptor);
+    validator.validate({num: 11}, function(errors, fields) {
+      assert.equal(errors.length, 1);
+      assert.equal(errors[0].message, "Number may not be above 10");
       done();
     });
   });
