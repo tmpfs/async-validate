0fb5a8e5947c139ccb572cdf821efa3052090746
muji <freeformsystems@gmail.com>
2015-09-11 16:43:07 +0800

    Tidy test spec.

--- test/spec/message-object.js
+++ test/spec/message-object.js
@@ -1,42 +1,38 @@
-var util = require('util');
-var assert = require('chai').assert;
-var schema = require('../../index');
-var msg = require('../../messages');
+var expect = require('chai').expect
+  , schema = require('../../index');
 
 describe("async-validate:", function() {
 
-  it("should validate using a custom error message as object", function(done) {
+  it("should validate using a custom error message as min", function(done) {
     var descriptor = {
       num: {
         type: "number", min: 0, max: 10,
         message: {
           min: 'Number may not be below zero',
-          max: 'Number may not be above 10',
+          max: 'Number may not be above ten',
         }
       }
     }
     var validator = new schema(descriptor);
     validator.validate({num: -1}, function(errors, fields) {
-      assert.equal(errors.length, 1);
-      assert.equal(errors[0].message, "Number may not be below zero");
+      expect(errors[0].message).to.eql("Number may not be below zero");
       done();
     });
   });
 
-  it("should validate using a custom error message as object", function(done) {
+  it("should validate using a custom error message as max", function(done) {
     var descriptor = {
       num: {
         type: "number", min: 0, max: 10,
         message: {
           min: 'Number may not be below zero',
-          max: 'Number may not be above 10',
+          max: 'Number may not be above ten',
         }
       }
     }
     var validator = new schema(descriptor);
     validator.validate({num: 11}, function(errors, fields) {
-      assert.equal(errors.length, 1);
-      assert.equal(errors[0].message, "Number may not be above 10");
+      expect(errors[0].message).to.eql("Number may not be above ten");
       done();
     });
   });
