8057eb1204db34d99a8a8fb0c249a7baa3aa5881
muji <freeformsystems@gmail.com>
2015-09-13 07:44:17 +0800

    Tidy null type plugin.

--- plugin/null.js
+++ plugin/null.js
@@ -2,7 +2,11 @@ module.exports = function() {
   this.main.null = function validate(cb) {
     if(this.shouldValidate()) {
       this.required();
-      this.type();
+      if(this.value !== null) {
+        this.raise(
+          this.reasons.type,
+          this.messages.types[this.rule.type], this.field, this.rule.type);
+      } 
     }
     cb();
   }
