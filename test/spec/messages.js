b737419ebafd758fb7a58f47bac91c16dec5c10b
muji <freeformsystems@gmail.com>
2015-09-12 11:54:20 +0800

    Rule message may be a function.

--- test/spec/messages.js
+++ test/spec/messages.js
@@ -1,8 +1,13 @@
-var assert = require('chai').assert;
-var schema = require('../../index');
-var msg = require('../../messages');
+var expect = require('chai').expect
+  , schema = require('../../index')
+  , msg = require('../../messages');
 
 describe("async-validate:", function() {
+
+  // clone of the default messages
+  var clone = schema.clone(msg);
+  // change a message
+  clone.required = "%s is a required field";
 
   it("should validate using a custom error message", function(done) {
     var descriptor = {
@@ -10,17 +15,32 @@ describe("async-validate:", function() {
     }
     var validator = new schema(descriptor);
     validator.validate({}, function(errors, fields) {
-      assert.equal(errors.length, 1);
-      assert.equal(errors[0].message, "Name is required");
+      expect(errors.length).to.eql(1);
+      expect(errors[0].message).to.eql("Name is required");
       done();
     });
   });
 
-  // clone of the default messages
-  var clone = schema.clone(msg);
-
-  // change a message
-  clone.required = "%s is a required field";
+  it("should validate using a custom error message function", function(done) {
+    var descriptor = {
+      name: {
+        type: "string",
+        required: true,
+        message: function(message, parameters) {
+          expect(message).to.be.a('string');
+          expect(parameters).to.be.an('array');
+          expect(this.reason()).to.eql(this.reasons.required);
+          return 'Name is required';
+        }
+      },
+    }
+    var validator = new schema(descriptor);
+    validator.validate({}, function(errors, fields) {
+      expect(errors.length).to.eql(1);
+      expect(errors[0].message).to.eql("Name is required");
+      done();
+    });
+  });
 
   it("should validate using custom messages", function(done) {
     var descriptor = {
@@ -32,13 +52,13 @@ describe("async-validate:", function() {
     validator.messages(clone);
 
     validator.validate({}, function(errors, fields) {
-      assert.equal(errors.length, 1);
-      assert.equal(errors[0].message, "name is a required field");
+      expect(errors.length).to.eql(1);
+      expect(errors[0].message).to.eql("name is a required field");
       done();
     });
   });
 
-  it("should verify custom error message helper", function(done) {
+  it("should use raise() helper method", function(done) {
     var descriptor = {
       name: function(cb) {
         this.raise("%s is a required field", this.field);
@@ -47,8 +67,8 @@ describe("async-validate:", function() {
     }
     var validator = new schema(descriptor);
     validator.validate({}, function(errors, fields) {
-      assert.equal(errors.length, 1);
-      assert.equal(errors[0].message, "name is a required field");
+      expect(errors.length).to.eql(1);
+      expect(errors[0].message).to.eql("name is a required field");
       done();
     });
   });
