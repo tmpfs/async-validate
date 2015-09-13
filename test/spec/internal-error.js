9e041cf7c5b5e9680085083e6a2d6ea6c179d2e6
muji <freeformsystems@gmail.com>
2015-09-13 21:07:52 +0800

    Assert on passing error to rule callback.

---
+++ test/spec/internal-error.js
@@ -0,0 +1,27 @@
+var expect = require('chai').expect
+  , Schema = require('../../index');
+
+describe('async-validate:', function() {
+
+  var descriptor = {
+    mock: function(cb) {
+      cb(new Error('query error'));  
+    },
+    next: function(cb) {
+      throw new Error('rule validation function invoked unexpectedly'); 
+    }
+  }
+
+  it('should callback with error', function(done) {
+    var schema = new Schema(descriptor);
+    schema.validate({}, function(err, res) {
+      function fn() {
+        throw err;
+      }
+      expect(fn).throws(Error);
+      expect(fn).throws(/query error/);
+      done();
+    });
+  });
+
+});
