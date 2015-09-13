56a3eebdfd0790c7b7cc3ab28f337d1b1fc312c6
muji <freeformsystems@gmail.com>
2015-09-13 09:56:46 +0800

    Update multiple test spec.

--- test/spec/multiple.js
+++ test/spec/multiple.js
@@ -1,60 +1,69 @@
-var assert = require('chai').assert
-  , schema = require('../../index')
+var expect = require('chai').expect
+  , Schema = require('../../index')
   , email = /^.+@.+\..+/;
 
 describe("async-validate:", function(done) {
 
-  it("should validate using multiple validation rules for a field", function(done) {
-    var descriptor = {
-      email: [
-        {type: "string", required: true},
-        {pattern: email, required: true}
-      ]
+  it("should error on multiple validation rules for a field",
+    function(done) {
+      var descriptor = {
+        email: [
+          {type: "string", required: true},
+          {pattern: email, required: true}
+        ]
+      }
+      var schema = new Schema(descriptor);
+      schema.validate({address: "joe@example"}, function(errors, fields) {
+        expect(errors.length).to.eql(2);
+        expect(errors[0].message).to.eql('email is required');
+        expect(errors[1].message).to.eql(
+          'email value undefined does not match pattern ' + email);
+        done();
+      });
     }
-    var validator = new schema(descriptor);
-    validator.validate({address: "joe@example"}, function(errors, fields) {
-      assert.equal(errors.length, 2);
-      assert.equal(errors[0].message, "email is required");
-      assert.equal(errors[1].message,
-        "email value undefined does not match pattern " + email);
-      done();
-    });
-  });
-  it("should validate using multiple validation rules for a field single failure", function(done) {
-    var descriptor = {
-      email: [
-        {type: "string", required: true},
-        {pattern: email}
-      ]
+  );
+  it("should error on multiple validation rules for a field single failure",
+    function(done) {
+      var descriptor = {
+        email: [
+          {type: "string", required: true},
+          {pattern: email}
+        ]
+      }
+      var schema = new Schema(descriptor);
+      schema.validate({email: "user@example"}, function(errors, fields) {
+        expect(errors.length).to.eql(1);
+        expect(errors[0].message).to.eql(
+          'email value user@example does not match pattern ' + email);
+        done();
+      });
     }
-    var validator = new schema(descriptor);
-    validator.validate({email: "user@example"}, function(errors, fields) {
-      assert.equal(errors.length, 1);
-      assert.equal(errors[0].message,
-        "email value user@example does not match pattern " + email);
-      done();
-    });
-  });
-  it("should validate using multiple validation rules with a validation function", function(done) {
-    var descriptor = {
-      email: [
-        {type: 'string', pattern: email, required: true},
-        function(cb) {
-          var email = "user@example.com";
-          if(this.value === email) {
-            this.raise("Email address %s already exists", email);
+  );
+
+  it("should error on multiple validation rules with a validation function",
+    function(done) {
+      var descriptor = {
+        email: [
+          {type: 'string', pattern: email, required: true},
+          function(cb) {
+            var email = "user@example.com";
+            if(this.value === email) {
+              this.raise("Email address %s already exists", email);
+            }
+            cb();
           }
-          cb();
-        }
-      ]
+        ]
+      }
+      var schema = new Schema(descriptor)
+        , source = {email: "user@example.com"};
+
+      schema.validate(source, function(errors, fields) {
+        expect(errors.length).to.eql(1);
+        expect(errors[0].message).to.eql(
+          'Email address user@example.com already exists');
+        done();
+      });
     }
-    var validator = new schema(descriptor);
-    validator.validate({email: "user@example.com"}, function(errors, fields) {
-      assert.equal(errors.length, 1);
-      assert.equal(errors[0].message,
-        "Email address user@example.com already exists");
-      done();
-    });
-  });
+  );
 
 });
