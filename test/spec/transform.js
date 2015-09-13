8c3ba09bc6e9aabcf8be87e5044e75e3fd237885
muji <freeformsystems@gmail.com>
2015-09-13 09:44:50 +0800

    Update transform test spec.

--- test/spec/transform.js
+++ test/spec/transform.js
@@ -1,24 +1,28 @@
-var assert = require('chai').assert;
-var schema = require('../../index');
+var expect = require('chai').expect
+  , Schema = require('../../index');
 
 describe("async-validate:", function() {
-  it("should transform by stripping whitespace", function(done) {
-    var descriptor = {
-      name: {
-        type: "string",
-        required: true, pattern: /^[a-z]+$/,
-        transform: function(value) {
-          return value.trim();
-        }
+
+  var descriptor = {
+    name: {
+      type: "string",
+      required: true, pattern: /^[a-z]+$/,
+      transform: function(value) {
+        return value.trim();
       }
     }
-    var validator = new schema(descriptor);
-    var source = {name: " user  "};
-    validator.validate(source, function(errors, fields) {
-      assert.isNull(errors);
-      assert.isNull(fields);
-      assert.equal(source.name, "user");
+  }
+
+  it("should transform by stripping whitespace", function(done) {
+    var schema = new Schema(descriptor)
+      , source = {name: " user  "};
+
+    schema.validate(source, function(errors, fields) {
+      expect(errors).to.eql(null);
+      expect(fields).to.eql(null);
+      expect(source.name).to.eql('user');
       done();
     });
   });
+
 });
