bb8bdb0c00b1608a3981e439acc82d84f21b06df
muji <freeformsystems@gmail.com>
2015-09-13 10:08:11 +0800

    Update string test spec.

--- test/spec/string.js
+++ test/spec/string.js
@@ -1,20 +1,9 @@
-var assert = require('chai').assert;
-var schema = require('../../index');
+var expect = require('chai').expect
+  , Schema = require('../../index');
 
 describe("async-validate:", function() {
-  it("should validate a required string field is valid", function(done) {
-    var descriptor = {
-      name: {type: "string", required: true}
-    }
-    var validator = new schema(descriptor);
-    validator.validate({name: "field"}, function(errors, fields) {
-      assert.isNull(errors);
-      assert.isNull(fields);
-      done();
-    });
-  });
 
-  it("should validate using a custom validator function", function(done) {
+  it("should error on a custom schema function", function(done) {
     var descriptor = {
       name: function(cb) {
         if(!/^[a-z0-9]+$/.test(this.value)) {
@@ -25,173 +14,195 @@ describe("async-validate:", function() {
         cb();
       }
     }
-    var validator = new schema(descriptor);
-    validator.validate({name: "Firstname"}, function(errors, fields) {
-      assert.equal(errors.length, 1);
-      assert.equal(errors[0].message,
-        "name must be lowercase alphanumeric characters");
+    var schema = new Schema(descriptor);
+    schema.validate({name: "Firstname"}, function(errors, fields) {
+      expect(errors.length).to.eql(1);
+      expect(errors[0].message).to.eql(
+        'name must be lowercase alphanumeric characters');
       done();
     });
   });
-  it("should validate a required string field", function(done) {
+
+  it("should error on required string field", function(done) {
     var descriptor = {
       name: {type: "string", required: true}
     }
-    var validator = new schema(descriptor);
-    validator.validate({noname: "field"}, function(errors, fields) {
-      assert.equal(errors.length, 1);
-      assert.equal(errors[0].message, "name is required");
-      assert.equal(fields.name.length, 1);
+    var schema = new Schema(descriptor);
+    schema.validate({noname: "field"}, function(errors, fields) {
+      expect(errors.length).to.eql(1);
+      expect(fields.name.length).to.eql(1);
+      expect(errors[0].message).to.eql('name is required');
       done();
     });
   });
-  it("should validate a required string field (null)", function(done) {
+
+  it("should error on required string field (null)", function(done) {
     var descriptor = {
       name: {type: "string", required: true}
     }
-    var validator = new schema(descriptor);
-    validator.validate({name: null}, function(errors, fields) {
-      assert.equal(errors.length, 2);
-      assert.equal(errors[0].message, "name is required");
-      assert.equal(fields.name.length, 2);
+    var schema = new Schema(descriptor);
+    schema.validate({name: null}, function(errors, fields) {
+      expect(errors.length).to.eql(2);
+      expect(fields.name.length).to.eql(2);
+      expect(errors[0].message).to.eql('name is required');
       done();
     });
   });
-  it("should validate a field of type string is of the correct type", function(done) {
+
+  it("should error on non-string type", function(done) {
     var descriptor = {
       name: {type: "string"}
     }
-    var validator = new schema(descriptor);
-    validator.validate({name: 10}, function(errors, fields) {
-      assert.equal(errors.length, 1);
-      assert.equal(errors[0].message, "name is not a string");
+    var schema = new Schema(descriptor);
+    schema.validate({name: 10}, function(errors, fields) {
+      expect(errors.length).to.eql(1);
+      expect(errors[0].message).to.eql('name is not a string');
       done();
     });
   });
-  it("should validate a required string field with minimum length", function(done) {
-    var descriptor = {
-      name: {type: "string", required: true, min: 8}
-    }
-    var validator = new schema(descriptor);
-    validator.validate({name: "field"}, function(errors, fields) {
-      assert.equal(errors.length, 1);
-      assert.equal(errors[0].message, "name must be at least 8 characters");
-      done();
-    });
-  });
-  it("should validate a required string field with maximum length", function(done) {
-    var descriptor = {
-      name: {type: "string", required: true, max: 2}
-    }
-    var validator = new schema(descriptor);
-    validator.validate({name: "field"}, function(errors, fields) {
-      assert.equal(errors.length, 1);
-      assert.equal(errors[0].message, "name cannot be longer than 2 characters");
-      done();
-    });
-  });
-  it("should validate a required string field is valid and in the range", function(done) {
+
+  it("should error on required string field with minimum length",
+    function(done) {
       var descriptor = {
-        name: {type: "string", required: true, min: 6, max: 20}
+        name: {type: "string", required: true, min: 8}
       }
-      var validator = new schema(descriptor);
-      validator.validate({name: "valid field"}, function(errors, fields) {
-        assert.isNull(errors);
-        assert.isNull(fields);
+      var schema = new Schema(descriptor);
+      schema.validate({name: "field"}, function(errors, fields) {
+        expect(errors.length).to.eql(1);
+        expect(errors[0].message).to.eql(
+          'name must be at least 8 characters');
         done();
       });
     }
   );
-  it("should validate a required string field is less than a length range",
+
+  it("should error on required string field with maximum length",
+    function(done) {
+      var descriptor = {
+        name: {type: "string", required: true, max: 2}
+      }
+      var schema = new Schema(descriptor);
+      schema.validate({name: "field"}, function(errors, fields) {
+        expect(errors.length).to.eql(1);
+        expect(errors[0].message).to.eql(
+          'name cannot be longer than 2 characters');
+        done();
+      });
+    }
+  );
+
+  it("should error on required string field is less than a length range",
     function(done) {
       var descriptor = {
         name: {type: "string", required: true, min: 6, max: 8}
       }
-      var validator = new schema(descriptor);
-      validator.validate({name: "field"}, function(errors, fields) {
-        assert.equal(errors.length, 1);
-        assert.equal(
-          errors[0].message,
-          "name must be between 6 and 8 characters");
+      var schema = new Schema(descriptor);
+      schema.validate({name: "field"}, function(errors, fields) {
+        expect(errors.length).to.eql(1);
+        expect(errors[0].message).to.eql(
+          'name must be between 6 and 8 characters');
         done();
       });
     }
   );
-  it("should validate a required string field is greater than a length range",
+
+  it("should error on required string field is greater than a length range",
     function(done) {
       var descriptor = {
         name: {type: "string", required: true, min: 2, max: 4}
       }
-      var validator = new schema(descriptor);
-      validator.validate({name: "field"}, function(errors, fields) {
-        assert.equal(errors.length, 1);
-        assert.equal(
-          errors[0].message,
-          "name must be between 2 and 4 characters");
+      var schema = new Schema(descriptor);
+      schema.validate({name: "field"}, function(errors, fields) {
+        expect(errors.length).to.eql(1);
+        expect(errors[0].message).to.eql(
+          'name must be between 2 and 4 characters');
         done();
       });
     }
   );
-  it("should validate a regular expression pattern mismatch",
+  it("should error on regular expression pattern mismatch",
     function(done) {
       var descriptor = {
         name: {pattern: /^[0-9]+$/}
       }
-      var validator = new schema(descriptor);
-      validator.validate({name: "alpha"}, function(errors, fields) {
-        assert.equal(errors.length, 1);
-        //console.log(errors[0].message);
-        assert.equal(
-          errors[0].message,
-          "name value alpha does not match pattern /^[0-9]+$/");
+      var schema = new Schema(descriptor);
+      schema.validate({name: "alpha"}, function(errors, fields) {
+        expect(errors.length).to.eql(1);
+        expect(errors[0].message).to.eql(
+          'name value alpha does not match pattern /^[0-9]+$/');
         done();
       });
     }
   );
-  it("should validate a string consisting of whitespace",
+
+  it("should error on string consisting of whitespace",
     function(done) {
       var descriptor = {
         name: {type: "string", whitespace: true}
       }
-      var validator = new schema(descriptor);
-      validator.validate({name: "   "}, function(errors, fields) {
-        assert.equal(errors.length, 1);
-        //console.log(errors[0].message);
-        assert.equal( errors[0].message, "name cannot be empty");
+      var schema = new Schema(descriptor);
+      schema.validate({name: "   "}, function(errors, fields) {
+        expect(errors.length).to.eql(1);
+        expect(errors[0].message).to.eql('name cannot be empty');
         done();
       });
     }
   );
-  it("should validate the empty string",
+
+  it("should error on empty string",
     function(done) {
       var descriptor = {
         name: {type: "string", required: true, whitespace: true}
       }
-      var validator = new schema(descriptor);
-      validator.validate({name: ""}, function(errors, fields) {
-        assert.equal(errors.length, 1);
-        //console.log(errors[0].message);
-        assert.equal( errors[0].message, "name cannot be empty");
+      var schema = new Schema(descriptor);
+      schema.validate({name: ""}, function(errors, fields) {
+        expect(errors.length).to.eql(1);
+        expect(errors[0].message).to.eql('name cannot be empty');
         done();
       });
     }
   );
-  it("should revalidate after failure",
+
+  it("should validate on required string field", function(done) {
+    var descriptor = {
+      name: {type: "string", required: true}
+    }
+    var schema = new Schema(descriptor);
+    schema.validate({name: "field"}, function(errors, fields) {
+      expect(errors).to.eql(null);
+      done();
+    });
+  });
+
+
+  it("should validate on required string field in range",
+    function(done) {
+      var descriptor = {
+        name: {type: "string", required: true, min: 6, max: 20}
+      }
+      var schema = new Schema(descriptor);
+      schema.validate({name: "valid field"}, function(errors, fields) {
+        expect(errors).to.eql(null);
+        done();
+      });
+    }
+  );
+
+  it("should validate after failure",
     function(done) {
       var descriptor = {
         name: {type: "string", required: true, whitespace: true}
       }
-      var validator = new schema(descriptor);
-      validator.validate({name: ""}, function(errors, fields) {
-        assert.equal(errors.length, 1);
-        assert.equal( errors[0].message, "name cannot be empty");
-        validator.validate({name: "user"}, function(errors, fields) {
-          //console.log("after revalidation %j", errors);
-          assert.isNull(errors);
-          assert.isNull(fields);
+      var schema = new Schema(descriptor);
+      schema.validate({name: ""}, function(errors, fields) {
+        expect(errors.length).to.eql(1);
+        expect(errors[0].message).to.eql('name cannot be empty');
+        schema.validate({name: "user"}, function(errors, fields) {
+          expect(errors).to.eql(null);
           done();
         });
       });
     }
   );
+
 });
