d68087ed34bedb3f3501e71f73035d19b4a81a11
muji <freeformsystems@gmail.com>
2015-09-15 12:37:29 +0800

    Add type example.

---
+++ doc/example/type.js
@@ -0,0 +1,14 @@
+// validate a field type
+var Schema = require('../..')
+  , descriptor = {
+      flag: {type: 'boolean', required: true}
+    }
+  , source = {flag: 'foo'}
+  , schema;
+
+require('../../plugin/all');
+
+schema = new Schema(descriptor);
+schema.validate(source, function(err, res) {
+  console.dir(res.errors);
+});
