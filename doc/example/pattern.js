556a9d6935675ec33d4004f81ca5b99b3e39955d
muji <freeformsystems@gmail.com>
2015-09-15 12:38:49 +0800

    Add pattern example.

---
+++ doc/example/pattern.js
@@ -0,0 +1,14 @@
+// validate a field as matching a pattern
+var Schema = require('../..')
+  , descriptor = {
+    name: {type: 'string', required: true, pattern: /^[a-z0-9-]$/i}
+    }
+  , source = {name: '-name'}
+  , schema;
+
+require('../../plugin/all');
+
+schema = new Schema(descriptor);
+schema.validate(source, function(err, res) {
+  console.dir(res.errors);
+});
