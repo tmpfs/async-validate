56600415460961f0619f00e11dc43c9597a0888d
muji <freeformsystems@gmail.com>
2015-09-17 17:12:09 +0800

    Draft match test spec.
    Rename method test spec.

---
+++ test/spec/match.js
@@ -0,0 +1,37 @@
+var expect = require('chai').expect
+  , Schema = require('../../index');
+
+describe('async-validate:', function() {
+
+  var descriptor = {
+      type: 'object',
+      required: true,
+      name: {type: 'string', required: true},
+      all: {
+        match: /./,
+        type: 'string'
+      }
+    }
+  , source = {address1: 'foo', address2: 'bar', address3: false};
+
+  it('should error on invalid expanded property (match)', function(done) {
+    var schema = new Schema(descriptor);
+    schema.validate(source, function(err, res) {
+      console.dir(res);
+      expect(res.errors.length).to.eql(1);
+      expect(res.errors[0].message).to.eql('address3 is not a string');
+      done();
+    });
+  });
+
+  it('should validate on expanded properties', function(done) {
+    var schema = new Schema(descriptor);
+    source.address3 = 'qux';
+    schema.validate(source, function(err, res) {
+      expect(err).to.eql(null);
+      expect(res).to.eql(null);
+      done();
+    });
+  });
+
+});
