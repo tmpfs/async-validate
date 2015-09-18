7f924379502e4d2a3493f4d7b3664f7e2a3a4859
muji <freeformsystems@gmail.com>
2015-09-18 12:38:53 +0800

    Update system schema.

--- system.js
+++ system.js
@@ -2,25 +2,23 @@
 var schema = {
   type: 'object',
   required: true,
-  all: {
-    match: /./,
-    fields: {
-      additional: {type: 'boolean'},
-      fields: {type: ['object', 'array']},
-      format: {type: 'string'},
-      len: {type: 'integer'},
-      list: {type: 'array'},
-      local: {type: 'boolean'},
-      min: {type: 'integer'},
-      max: {type: 'integer'},
-      pattern: {type: 'regexp'},
-      placeholder: {type: 'function'},
-      required: {type: 'boolean'},
-      test: {type: 'function'},
-      type: {type: ['string', 'function'], required: true},
-      values: {type: ['object', 'array']},
-      whitespace: {type: 'boolean'}
-    }
+  fields: {
+    additional: {type: 'boolean'},
+    fields: {type: ['object', 'array']},
+    format: {type: 'string'},
+    len: {type: 'integer'},
+    list: {type: 'array'},
+    local: {type: 'boolean'},
+    min: {type: 'integer'},
+    max: {type: 'integer'},
+    match: {type: 'regexp'},
+    pattern: {type: 'regexp'},
+    placeholder: {type: 'function'},
+    required: {type: 'boolean'},
+    test: {type: 'function'},
+    type: {type: ['string', 'function'], required: true},
+    values: {type: ['object', 'array']},
+    whitespace: {type: 'boolean'}
   }
 }
 
