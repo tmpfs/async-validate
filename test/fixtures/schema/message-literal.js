afbd05df5928ef9c8e0c1799118e3d1c242a0a22
muji <freeformsystems@gmail.com>
2016-04-11 10:24:48 +0800

    Update literal test spec.

---
+++ test/fixtures/schema/message-literal.js
@@ -0,0 +1,12 @@
+var util = require('util');
+var schema = {
+  type: 'object',
+  fields: {
+    name: {type: 'string', required: true},
+    job: {type: 'string', required: true, message: function(msg, params) {
+      return util.format.apply(util, [msg].concat(params)); 
+    }}
+  }
+}
+
+module.exports = schema;
--- test/spec/message-literal.js
+++ test/spec/message-literal.js
@@ -1,12 +1,12 @@
 var expect = require('chai').expect
   , Schema = require('../../index')
   , msg = require('../../messages')
-  , override = require('../fixtures/schema/message-string-override');
+  , literal = require('../fixtures/schema/message-literal');
 
 describe('async-validate:', function() {
 
   it('should validate using literal option', function(done) {
-    var schema = new Schema(override);
+    var schema = new Schema(literal);
 
     // clone of the default messages
     var clone = Schema.clone(msg);
@@ -18,8 +18,9 @@ describe('async-validate:', function() {
     schema.messages(clone);
 
     schema.validate({}, {literal: true}, function(err, res) {
-      expect(res.errors.length).to.eql(1);
+      expect(res.errors.length).to.eql(2);
       expect(res.errors[0].message).to.eql('REQUIRED !');
+      expect(res.errors[1].message).to.eql('REQUIRED ! job');
       done();
     });
   });
